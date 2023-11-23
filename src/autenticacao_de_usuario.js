1b6870d1c29ec0923291a68b7cee631251002d48
Kletion Paulino da Silva <kleitonpaulinodasilva@gmail.com>
2023-11-23 11:43:10 -0300

    Install jsonwebtoken, body-parser and uuid

--- src/autenticacao_de_usuario.js
+++ src/autenticacao_de_usuario.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
+const bodyParser = require("body-parser");
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
+
+app.use(bodyParser.json());
 
 app.get("/", (request, response) =>{
     response.json({Teste: "Testando ambiente"})
 });
 
-app.listen(3000)
+app.listen(3000);
